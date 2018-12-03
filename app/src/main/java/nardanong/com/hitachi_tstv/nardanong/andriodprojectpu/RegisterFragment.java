1859e57e5504bbc529f6aa71f52887d01277bb3a
Nardanong.s <Nardanong.s@gmail.com>
2018-12-03 14:14:30 +0700

    Create Toolbar Success

--- app/src/main/java/nardanong/com/hitachi_tstv/nardanong/andriodprojectpu/RegisterFragment.java
+++ app/src/main/java/nardanong/com/hitachi_tstv/nardanong/andriodprojectpu/RegisterFragment.java
@@ -2,7 +2,9 @@ package nardanong.com.hitachi_tstv.nardanong.andriodprojectpu;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,6 +20,21 @@ public class RegisterFragment extends Fragment {
         // Required empty public constructor
     }
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        Create Toolbar
+        createToolbar();
+    }//Main Method
+
+    private void createToolbar() {
+        Toolbar toolbar = getView().findViewById(R.id.toolbarRegister);
+        ((MainActivity)getActivity()).setSupportActionBar(toolbar);
+        ((MainActivity) getActivity()).getSupportActionBar().setTitle(getString(R.string.register));
+        ((MainActivity) getActivity()).getSupportActionBar().setSubtitle("Please Fill All Balnk");
+
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -25,5 +42,4 @@ public class RegisterFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_register, container, false);
     }
-
-}
+}//Main Class
