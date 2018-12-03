afdabcb52d7ec6c951c5ffe14ea82ab3eea4d082
Nardanong.s <Nardanong.s@gmail.com>
2018-12-03 11:45:10 +0700

    Add Fragment Success

--- app/src/main/java/nardanong/com/hitachi_tstv/nardanong/andriodprojectpu/MainActivity.java
+++ app/src/main/java/nardanong/com/hitachi_tstv/nardanong/andriodprojectpu/MainActivity.java
@@ -6,8 +6,13 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) { //Save Process
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+//        Add Fragment to Activity
+        if(savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().add(R.id.contentMainFragment,new MainFragment()).commit();
+        }
+    }//Main Method
+} //Main Class
