cb4e7fd014437b612426802db4e489f2f12b7934
Nardanong.s <Nardanong.s@gmail.com>
2018-12-04 13:43:44 +0700

    Get json Success

--- app/src/main/java/nardanong/com/hitachi_tstv/nardanong/andriodprojectpu/MyConstant.java
+++ app/src/main/java/nardanong/com/hitachi_tstv/nardanong/andriodprojectpu/MyConstant.java
@@ -5,6 +5,12 @@ public class MyConstant {
     private String urlAddUser = "http://androidthai.in.th/hit/addUserPu.php";
     private String urlPreAvartar = "http://androidthai.in.th/hit/pu";
 
+    public String getUrlGetAllUser() {
+        return urlGetAllUser;
+    }
+
+    private String urlGetAllUser = "http://androidthai.in.th/hit/getAllUserPu.php";
+
     public String getUrlPreAvartar() {
         return urlPreAvartar;
     }
